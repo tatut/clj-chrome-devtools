10b766df1e94f9997e9cce4eaec563bfdc84e134
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-08-20 17:02:55 +0300

    Add description, license and URL to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject clj-chrome-devtools "0.1-SNAPSHOT"
+  :description "Clojure API for Chrome DevTools remote"
+  :license {:name "MIT License"}
+  :url "https://github.com/tatut/clj-chrome-devtools"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [http-kit "2.2.0"]
                  [cheshire "5.8.0"]
