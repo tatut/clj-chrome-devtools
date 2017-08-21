e2f698043409edb6687417b09b5e61868116cce4
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-08-21 09:25:55 +0300

    Add codox documentation

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
                  [http-kit "2.2.0"]
                  [cheshire "5.8.0"]
                  [stylefruits/gniazdo "1.0.1"]
-                 [org.clojure/core.async "0.3.443"]])
+                 [org.clojure/core.async "0.3.443"]]
+  :plugins [[lein-codox "0.10.3"]]
+  :codox {:output-path "docs/api"})
