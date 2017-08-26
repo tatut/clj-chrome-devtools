8fa106c957d9bd361c92a51226690820b26e8d17
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-08-26 10:21:46 +0300

    Test selectors and text input

--- test/clj_chrome_devtools/chrome_test.clj
+++ test/clj_chrome_devtools/chrome_test.clj
@@ -2,14 +2,32 @@
   "Some tests with an actual running Chrome"
   (:require [clj-chrome-devtools.automation.fixture :refer [create-chrome-fixture]]
             [clj-chrome-devtools.automation :refer :all]
-            [clojure.test :as t :refer [deftest is]]
+            [clojure.test :as t :refer [deftest is testing]]
             [clojure.java.io :as io]))
 
-(t/use-fixtures :each (create-chrome-fixture))
+(defonce chrome-fixture (create-chrome-fixture))
+(t/use-fixtures :each chrome-fixture)
 
-;; Very primitive test to just check that chrome is up and running
-;; and can inspect a page.
+(defn load-test-page []
+  (to (str "file://" (.getAbsolutePath (io/file "test/test-page.html")))))
 
 (deftest simple-page-load
-  (to (str "file://" (.getAbsolutePath (io/file "test/test-page.html"))))
+  (load-test-page)
   (is (= (map text-of (sel "ul#thelist li")) '("foo" "bar" "baz"))))
+
+(deftest selectors
+  (load-test-page)
+  (testing "Selectors work in place of node-refences"
+    (is (= "0" (text-of "#counter")))
+    (click [:div.countertest :button.increment])
+    (is (= "1" (text-of [:#counter])))
+    (click (sel1 "button.increment"))
+    (is (= "2" (text-of "div#counter")))))
+
+(deftest input
+  (load-test-page)
+  (testing "Typing into an input field works"
+    (is (= "NO GREETING YET" (text-of "#greeting")))
+    (input-text [:#greeter :input] "old friend")
+    (click "#greeter button")
+    (is (= "Hello, old friend!" (text-of [:#greeting])))))
