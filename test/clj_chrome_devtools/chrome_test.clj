424046e4e361acf284b970b9c8ff886683b8de41
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-08-26 09:06:25 +0300

    Use local test page for tests

--- test/clj_chrome_devtools/chrome_test.clj
+++ test/clj_chrome_devtools/chrome_test.clj
@@ -2,7 +2,8 @@
   "Some tests with an actual running Chrome"
   (:require [clj-chrome-devtools.automation.fixture :refer [create-chrome-fixture]]
             [clj-chrome-devtools.automation :refer :all]
-            [clojure.test :as t :refer [deftest is]]))
+            [clojure.test :as t :refer [deftest is]]
+            [clojure.java.io :as io]))
 
 (t/use-fixtures :each (create-chrome-fixture))
 
@@ -10,5 +11,5 @@
 ;; and can inspect a page.
 
 (deftest simple-page-load
-  (to "http://webjure.org/clj-chrome-devtools.html")
+  (to (str "file://" (.getAbsolutePath (io/file "test/test-page.html"))))
   (is (= (map text-of (sel "ul#thelist li")) '("foo" "bar" "baz"))))
