d07dcb1cf86726124cde5d651b1f3390f6da3b88
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2018-03-10 10:11:57 +0200

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-chrome-devtools "20180310-SNAPSHOT"
+(defproject clj-chrome-devtools "20180310"
   :description "Clojure API for Chrome DevTools remote"
   :license {:name "MIT License"}
   :url "https://github.com/tatut/clj-chrome-devtools"
