d841f343bbbf46ee0b067ce61714d8ccb5465e0d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-09 22:37:34 +0300

    Fix old names

--- src/clj_chrome_devtools/commands/domdebugger.clj
+++
@@ -1,4 +0,0 @@
-(ns clj-chrome-devtools.commands.domdebugger
-  "DOM debugging allows setting breakpoints on particular DOM operations and events. JavaScript execution will stop on these operations as if there was a regular breakpoint set."
-  (:require [clj-chrome-devtools.impl.define :refer [define-domain]]))
-(define-domain "DOMDebugger")
--- src/clj_chrome_devtools/commands/domstorage.clj
+++
@@ -1,4 +0,0 @@
-(ns clj-chrome-devtools.commands.domstorage
-  "Query and modify DOM storage."
-  (:require [clj-chrome-devtools.impl.define :refer [define-domain]]))
-(define-domain "DOMStorage")
