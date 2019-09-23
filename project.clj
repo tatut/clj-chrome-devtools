545291d813a917f516b9d47f597f72584c220575
Avi Flax <avi.flax@fundingcircle.com>
2019-09-23 18:59:51 -0400

    Adopt lein-tools-deps
    Before this the dependencies were declared redundantly in two different
    places (`project.clj` and `deps.edn`) and these two declarations had to
    be kept in sync manually — which was bound to fail (via human error)
    sooner or later.
    
    lein-tools-deps allows us to specify the dependencies in one place while
    ensuring that both tools.deps and Leiningen will continue to work with
    this project.
    
    https://github.com/RickMoynihan/lein-tools-deps

--- project.clj
+++ project.clj
@@ -2,12 +2,9 @@
   :description "Clojure API for Chrome DevTools remote"
   :license {:name "MIT License"}
   :url "https://github.com/tatut/clj-chrome-devtools"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [http-kit "2.3.0"]
-                 [cheshire "5.8.1"]
-                 [stylefruits/gniazdo "1.1.1"]
-                 [org.clojure/core.async "0.4.490"]
-                 [com.taoensso/timbre "4.10.0"]]
-  :plugins [[lein-codox "0.10.3"]]
+  :plugins [[lein-codox "0.10.3"]
+            [lein-tools-deps "0.4.5"]]
   :codox {:output-path "docs/api"
-          :metadata {:doc/format :markdown}})
+          :metadata {:doc/format :markdown}}
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
